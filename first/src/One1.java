e189a47573cd6d04477f4885bb401674602975e6
songzhixin <songzhixin.szx@163.com>
2017-07-05 12:28:54 +0800

    idea的第一次提交

---
+++ first/src/One1.java
@@ -0,0 +1,16 @@
+/**
+ * Created by wuwei on 2017-07-05.
+ * copyright @ Aisino
+ * E-mail:songzhixin@aisino.com
+ */
+public class One1{
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(2222);
+    }
+    public String f(String p){
+        return 1+"";
+    }
+}
+
