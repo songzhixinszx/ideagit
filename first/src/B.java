8009849b50ccc530d4be78c5631f33a6d568bbad
songzhixinszx <songzhixin.szx@163.com>
2017-07-05 16:34:01 +0800

    Update B.java

--- first/src/B.java
+++ first/src/B.java
@@ -6,12 +6,17 @@
 public class B {
 
     public static void main(String[] args) {
+        
         System.out.println(1);
         int bb;
         int cc;
+         System.out.println(1);
         int wg;
+         System.out.println(1);
         int BB;
+         System.out.println(1);
         int q;
        System.out.println(2);
+         System.out.println(1);
     }
 }
