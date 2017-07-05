e0e420bc4c6d2d8efa9f5318b4bc03a64fed2228
songzhixin <songzhixin.szx@163.com>
2017-07-05 16:26:50 +0800

    idea的bb

---
+++ first/src/B.java
@@ -0,0 +1,11 @@
+/**
+ * Created by wuwei on 2017-07-05.
+ * copyright @ Aisino
+ * E-mail:songzhixin@aisino.com
+ */
+public class B {
+
+    public static void main(String[] args) {
+        int bb;
+    }
+}
